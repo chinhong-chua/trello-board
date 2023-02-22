78b394f066d5b60384343da1bd05f28d9108d570
Cruz <cruz_ch96@outlook.com>
2023-02-22 18:11:51 +0800

    Commit Styling and Board

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10"><img width="100" class="mr-3" src="/Cruz-logos.jpeg" alt="Cruz Trello Board" />My Trello Board</h1>
     <TrelloBoard />
   </div>
 </template>
