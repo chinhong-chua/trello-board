05b1c0017e07379392c1c3d3ba7b8a47f3b9f6b2
Cruz <cruz_ch96@outlook.com>
2023-02-23 23:31:18 +0800

    commit drag handle & task

--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -7,9 +7,32 @@ defineProps<{
 </script>
 
 <template>
-  <div :title="task.createdAt.toLocaleDateString()" class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]">
+  <div
+    :title="task.createdAt.toLocaleDateString()"
+    class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+  >
+    <DragHandle class="pr-2" />
     <span>
       {{ task.title }}
     </span>
   </div>
 </template>
+
+<style>
+/* .sortable-chosen{
+background-color: green;
+} */
+
+.sortable-drag .task {
+  transform: rotate(5deg);
+}
+
+.sortable-ghost .task {
+  position: relative;
+}
+
+.sortable-ghost .task ::after {
+  content: "";
+  @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
+}
+</style>
